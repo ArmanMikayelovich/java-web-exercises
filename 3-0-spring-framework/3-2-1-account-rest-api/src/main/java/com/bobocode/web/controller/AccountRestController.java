a12c2b98ae4c1f04f8a0fa50428d7df75fd31050
Arman Arshakyan <arman.arshakyan@dataart.com>
2024-06-13 02:19:37 +0400

    #8 Spring, Spring MVC, Dispatcher Servlet Initializer, REST API, String Trimming

--- 3-0-spring-framework/3-2-1-account-rest-api/src/main/java/com/bobocode/web/controller/AccountRestController.java
+++ 3-0-spring-framework/3-2-1-account-rest-api/src/main/java/com/bobocode/web/controller/AccountRestController.java
@@ -1,6 +1,19 @@
 package com.bobocode.web.controller;
 
 import com.bobocode.dao.AccountDao;
+import com.bobocode.model.Account;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * <p>
@@ -16,6 +29,46 @@ import com.bobocode.dao.AccountDao;
  * todo: 7. Implement method that handles DELETE request with id as path variable removes an account by id
  * todo:    Configure HTTP response status code 204 - NO CONTENT
  */
+@RestController
+@RequestMapping("/accounts")
 public class AccountRestController {
 
+    private final AccountDao accountDao;
+
+    public AccountRestController(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    @GetMapping
+    public List<Account> getAll() {
+        return accountDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Account getById(@PathVariable Long id) {
+        return accountDao.findById(id);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Account> post(Account account) {
+        return new ResponseEntity<>(accountDao.save(account),HttpStatusCode.valueOf(201));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> put(@PathVariable Long id, Account account) {
+
+        if (!account.getId().equals(id)) {
+            throw new IllegalArgumentException();
+        }
+        return new ResponseEntity<>(HttpStatusCode.valueOf(204));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> remove(@PathVariable Long id) {
+        Account byId = accountDao.findById(id);
+        accountDao.remove(byId);
+        return new ResponseEntity<>(HttpStatusCode.valueOf(204));
+
+    }
 }
